7b583e50bbf646c2f0d223d94b7d95913802b5e1
Malebestia <vair81@gmail.com>
2023-09-18 12:19:00 +0200

    up

--- Filament/Pages/Dashboard.php
+++ Filament/Pages/Dashboard.php
@@ -13,4 +13,11 @@ class Dashboard extends Page
     protected static ?string $navigationIcon = 'heroicon-o-home';
 
     protected static string $view = 'job::filament.pages.dashboard';
+
+    public function mount(): void {
+        $user = auth()->user();
+        if(!$user->hasRole('super-admin')){
+            redirect('/admin');
+        }
+    }
 }
