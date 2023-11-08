e4a1de8940c662f9cbc537eaa0c95fe2dad94a72
marco76tv <marco.sottana@gmail.com>
2023-11-08 11:18:15 +0100

    up

--- Resources/views/livewire/job/status.blade.php
+++ Resources/views/livewire/job/status.blade.php
@@ -36,7 +36,7 @@
             <pre>{!! $out !!}</pre>
         </p>
 
-        <label for="countries" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">conn</label>
+        <x-filament-forms::field-wrapper.label for="countries" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">conn</label>
         <select id="countries" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
         wire.model.lazy="form_data.conn">
             <option selected>Choose a country</option>
