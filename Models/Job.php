7d257812cf2465c74558eb2b6788502972e60f6b
marco76tv <marco.sottana@gmail.com>
2023-11-22 10:52:33 +0100

    up

--- Models/Job.php
+++ Models/Job.php
@@ -16,30 +16,10 @@ use Webmozart\Assert\Assert;
 /**
  * Modules\Job\Models\Job
  *
- * @property int $id
- * @property string $queue
- * @property array $payload
- * @property int $attempts
- * @property int|null $reserved_at
- * @property int $available_at
- * @property Carbon $created_at
- * @property string|null $created_by
- * @property string|null $updated_by
- * @property Carbon|null $updated_at
  * @method static \Modules\Job\Database\Factories\JobFactory factory($count = null, $state = [])
  * @method static Builder|Job newModelQuery()
  * @method static Builder|Job newQuery()
  * @method static Builder|Job query()
- * @method static Builder|Job whereAttempts($value)
- * @method static Builder|Job whereAvailableAt($value)
- * @method static Builder|Job whereCreatedAt($value)
- * @method static Builder|Job whereCreatedBy($value)
- * @method static Builder|Job whereId($value)
- * @method static Builder|Job wherePayload($value)
- * @method static Builder|Job whereQueue($value)
- * @method static Builder|Job whereReservedAt($value)
- * @method static Builder|Job whereUpdatedAt($value)
- * @method static Builder|Job whereUpdatedBy($value)
  * @mixin \Eloquent
  */
 class Job extends BaseModel
