360d9633c8e8b9fba37ed7b594b44a11efd554c9
marco76tv <marco.sottana@gmail.com>
2023-11-27 16:19:24 +0100

    Remove unnecessary properties and methods from FailedJob model

--- Models/FailedJob.php
+++ Models/FailedJob.php
@@ -14,24 +14,10 @@ use Illuminate\Database\Eloquent\Builder;
 /**
  * Modules\Job\Models\FailedJob
  *
- * @property int $id
- * @property string $uuid
- * @property string $connection
- * @property string $queue
- * @property array $payload
- * @property string $exception
- * @property string $failed_at
  * @method static \Modules\Job\Database\Factories\FailedJobFactory factory($count = null, $state = [])
  * @method static Builder|FailedJob newModelQuery()
  * @method static Builder|FailedJob newQuery()
  * @method static Builder|FailedJob query()
- * @method static Builder|FailedJob whereConnection($value)
- * @method static Builder|FailedJob whereException($value)
- * @method static Builder|FailedJob whereFailedAt($value)
- * @method static Builder|FailedJob whereId($value)
- * @method static Builder|FailedJob wherePayload($value)
- * @method static Builder|FailedJob whereQueue($value)
- * @method static Builder|FailedJob whereUuid($value)
  * @mixin \Eloquent
  */
 class FailedJob extends BaseModel
