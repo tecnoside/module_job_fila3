84c283b147a9f72f27d03c475cb62caad489638d
marco76tv <marco.sottana@gmail.com>
2024-02-14 10:34:33 +0100

    conflict

--- Console/Commands/WorkerCheck.php
+++ Console/Commands/WorkerCheck.php
@@ -18,14 +18,9 @@ class WorkerCheck extends Command
 {
     /**
      * The name and signature of the console command.
-<<<<<<< HEAD
     * @var string
  */
     protected $signature = 'worker:check';
-=======
-     */
-    protected string $signature = 'worker:check';
->>>>>>> 6469dcd (up)
 
     /**
      * The console command description.
