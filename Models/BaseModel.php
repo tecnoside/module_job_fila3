6af1734b54a57b368f838d1f19b9ab7edf4fd6a0
marco76tv <marco.sottana@gmail.com>
2024-02-23 10:12:41 +0100

    up

--- Models/BaseModel.php
+++ Models/BaseModel.php
@@ -30,14 +30,10 @@ abstract class BaseModel extends Model
      */
     public static $snakeAttributes = true;
 
-    /**
-     * @var bool
-     */
+    /** @var bool */
     public $incrementing = true;
 
-    /**
-     * @var bool
-     */
+    /** @var bool */
     public $timestamps = true;
 
     /**
@@ -45,9 +41,7 @@ abstract class BaseModel extends Model
      */
     protected $perPage = 30;
 
-    /**
-     * @var string
-     */
+    /** @var string */
     protected $connection = 'mysql'; // this will use the specified database connection
 
     /**
@@ -65,14 +59,10 @@ abstract class BaseModel extends Model
         'deleted_at' => 'datetime',
     ];
 
-    /**
-     * @var string
-     */
+    /** @var string */
     protected $primaryKey = 'id';
 
-    /**
-     * @var array<int, string>
-     */
+    /** @var  array<int, string> */
     protected $hidden = [
         // 'password'
     ];
