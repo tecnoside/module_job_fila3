e87f2285daa24581c2b1bf3d3ad028d431699e50
marco76tv <marco.sottana@gmail.com>
2024-04-15 15:12:00 +0200

    up

--- Models/Import.php
+++ Models/Import.php
@@ -11,11 +11,42 @@ namespace Modules\Job\Models;
 use Illuminate\Database\Eloquent\Builder;
 
 /**
+ * 
+ *
  * @method static \Modules\Job\Database\Factories\ImportFactory factory($count = null, $state = [])
  * @method static Builder|Import                                newModelQuery()
  * @method static Builder|Import                                newQuery()
  * @method static Builder|Import                                query()
- *
+ * @property int $id
+ * @property \Illuminate\Support\Carbon|null $completed_at
+ * @property string $file_name
+ * @property string $file_path
+ * @property string $importer
+ * @property int $processed_rows
+ * @property int $total_rows
+ * @property int $successful_rows
+ * @property string|null $user_id
+ * @property \Illuminate\Support\Carbon|null $created_at
+ * @property \Illuminate\Support\Carbon|null $updated_at
+ * @property string|null $updated_by
+ * @property string|null $created_by
+ * @property string|null $deleted_at
+ * @property string|null $deleted_by
+ * @method static Builder|Import whereCompletedAt($value)
+ * @method static Builder|Import whereCreatedAt($value)
+ * @method static Builder|Import whereCreatedBy($value)
+ * @method static Builder|Import whereDeletedAt($value)
+ * @method static Builder|Import whereDeletedBy($value)
+ * @method static Builder|Import whereFileName($value)
+ * @method static Builder|Import whereFilePath($value)
+ * @method static Builder|Import whereId($value)
+ * @method static Builder|Import whereImporter($value)
+ * @method static Builder|Import whereProcessedRows($value)
+ * @method static Builder|Import whereSuccessfulRows($value)
+ * @method static Builder|Import whereTotalRows($value)
+ * @method static Builder|Import whereUpdatedAt($value)
+ * @method static Builder|Import whereUpdatedBy($value)
+ * @method static Builder|Import whereUserId($value)
  * @mixin \Eloquent
  */
 class Import extends BaseModel
--- Models/Result.php
+++ Models/Result.php
@@ -23,7 +23,6 @@ use Illuminate\Support\Facades\DB;
  * @property Carbon|null                   $created_at
  * @property Carbon|null                   $updated_at
  * @property \Modules\Job\Models\Task|null $task
- *
  * @method static \Illuminate\Database\Eloquent\Builder|Result newModelQuery()
  * @method static \Illuminate\Database\Eloquent\Builder|Result newQuery()
  * @method static \Illuminate\Database\Eloquent\Builder|Result query()
@@ -36,7 +35,6 @@ use Illuminate\Support\Facades\DB;
  * @method static \Illuminate\Database\Eloquent\Builder|Result whereTaskId($value)
  * @method static \Illuminate\Database\Eloquent\Builder|Result whereUpdatedAt($value)
  * @method static \Illuminate\Database\Eloquent\Builder|Result whereUpdatedBy($value)
- *
  * @mixin \Eloquent
  */
 class Result extends BaseModel
