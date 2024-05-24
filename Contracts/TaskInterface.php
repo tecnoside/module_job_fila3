3c8b798cc725afbdd31d82ba7dcd154debeaaae6
marco <marco.sottana@gmail.com>
2024-05-24 15:52:24 +0200

    up

--- Contracts/TaskInterface.php
+++ Contracts/TaskInterface.php
@@ -6,8 +6,12 @@ namespace Modules\Job\Contracts;
 
 use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Collection;
+use Illuminate\Database\Eloquent\Model;
 use Modules\Job\Models\Task;
 
+/**
+ * @phpstan-require-extends Model
+ */
 interface TaskInterface
 {
     /**
