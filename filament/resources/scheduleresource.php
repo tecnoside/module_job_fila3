0ad2490aa42e173e52236f66b95f7046a555c21a
Retype GitHub Action <retype@object.net>
2024-07-03 14:27:08 +0000

    Refreshes Retype-generated documentation.
    Process triggered by marco76tv.

--- filament/resources/scheduleresource.php
+++ filament/resources/scheduleresource.php
@@ -7,10 +7,10 @@ declare(strict_types=1);
 
 namespace Modules\Job\Filament\Resources;
 
-use Filament\Forms\Components\Section;
 use Filament\Forms\Components\CheckboxList;
 use Filament\Forms\Components\Hidden;
 use Filament\Forms\Components\Repeater;
+use Filament\Forms\Components\Section;
 use Filament\Forms\Components\Select;
 use Filament\Forms\Components\TagsInput;
 use Filament\Forms\Components\TextInput;
@@ -72,7 +72,7 @@ class ScheduleResource extends XotBaseResource
         $commands_opts = static::$commands->toCollection()->pluck('full_name', 'name')->toArray();
 
         return [
-            Card::make([
+            Section::make([
                 Select::make('command')
                     ->label(static::trans('fields.command'))
                     /*
@@ -100,7 +100,7 @@ class ScheduleResource extends XotBaseResource
                     ->placeholder(static::trans('messages.custom-command-here'))
                     ->label(static::trans('messages.custom'))
                     ->required()
-                    ->visible(fn (Get $get) => $get('command') === 'custom' && config('filament-database-schedule.commands.enable_custom')),
+                    ->visible(fn (Get $get) => 'custom' === $get('command') && config('filament-database-schedule.commands.enable_custom')),
                 Repeater::make('params')
                     ->schema([
                         Hidden::make('name'),
@@ -139,7 +139,7 @@ class ScheduleResource extends XotBaseResource
                 */
                 TextInput::make('expression')
                     ->placeholder('* * * * *')
-                    ->rules([new Corn])
+                    ->rules([new Corn()])
                     ->label(static::trans('fields.expression'))
                     ->required()
                     ->helperText(fn () => config('filament-database-schedule.tool-help-cron-expression.enable') ? new HtmlString(" <a href='".config('filament-database-schedule.tool-help-cron-expression.url')."' target='_blank'>".static::trans('messages.help-cron-expression').' </a>') : null),
