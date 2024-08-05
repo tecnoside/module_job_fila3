b73099320873a1c6227f73768f8708fa8d23043e
marco76tv <marco.sottana@gmail.com>
2024-08-05 17:56:34 +0200

    up

--- Filament/Resources/JobsWaitingResource/Pages/ListJobsWaiting.php
+++ Filament/Resources/JobsWaitingResource/Pages/ListJobsWaiting.php
@@ -11,10 +11,21 @@ namespace Modules\Job\Filament\Resources\JobsWaitingResource\Pages;
 use Filament\Resources\Pages\ListRecords;
 use Modules\Job\Filament\Resources\JobsWaitingResource;
 use Modules\Job\Filament\Resources\JobsWaitingResource\Widgets\JobsWaitingOverview;
+use Modules\UI\Enums\TableLayoutEnum;
+use Modules\UI\Filament\Actions\Table\TableLayoutToggleTableAction;
 
 class ListJobsWaiting extends ListRecords
 {
     public static string $resource = JobsWaitingResource::class;
+
+    public TableLayoutEnum $layoutView = TableLayoutEnum::GRID;
+
+    protected function getTableHeaderActions(): array
+    {
+        return [
+            TableLayoutToggleTableAction::make(),
+        ];
+    }
 
     public function getHeaderActions(): array
     {
