a46855b402b498856ad32ec761362b008e3fd5c6
marco76tv <marco.sottana@gmail.com>
2024-09-23 22:52:32 +0200

    up

--- Models/BaseModel.php
+++ Models/BaseModel.php
@@ -28,29 +28,22 @@ abstract class BaseModel extends Model
      * @var bool
      */
     public static $snakeAttributes = true;
-
     /** @var bool */
     public $incrementing = true;
-
     /** @var bool */
     public $timestamps = true;
-
     /** @var int */
     protected $perPage = 30;
-
     /** @var string */
     protected $connection = 'job';
-    protected $prefix = '_';
-
+    /** @var string|null */
+    protected $prefix = null;
     /** @var list<string> */
     protected $fillable = ['id'];
-
     /** @var string */
     protected $primaryKey = 'id';
-
     /** @var string */
     protected $keyType = 'string';
-
     /** @var list<string> */
     protected $hidden = [
         // 'password'
